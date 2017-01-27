dcc01c0c25a33cedbe1481a795d7eb0771390a7b
Angel Rojo Marijuan <arojomarijuan@magento.com>
2017-01-27 19:48:24 +0100

    refactor: replace recursion with loop-recur

--- src/factors/core.clj
+++ src/factors/core.clj
@@ -1,11 +1,9 @@
 (ns factors.core)
 
-(defn prime
-  ([int]
-    (prime int []))
-  ([int factors]
-   (if (< int 2)
-     factors
-     (if (= 0 (mod int 2))
-       (prime (quot int 2) (conj factors 2))
-       (conj factors int)))))
+(defn prime [int]
+  (loop [int int factors []]
+    (if (< int 2)
+      factors
+      (if (= 0 (mod int 2))
+        (recur (quot int 2) (conj factors 2))
+        (conj factors int)))))
