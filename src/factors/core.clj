a2e6d2175833cf86ec5e51ba266ae1633552e3aa
Angel Rojo Marijuan <arojomarijuan@magento.com>
2017-01-27 19:29:09 +0100

    refactor: introduce multi arity in preparation to recursion

--- src/factors/core.clj
+++ src/factors/core.clj
@@ -1,11 +1,14 @@
 (ns factors.core)
 
-(defn prime [int]
-  (if (> int 1)
-    (if (= 0 (mod int 2))
-      (let [new-int (quot int 2)]
-        (if (> new-int 1)
-          (conj (conj [] 2) new-int)
-          (conj [] 2)))
-      (conj [] int))
-    []))
+(defn prime
+  ([int]
+    (prime int []))
+  ([int factors]
+   (if (> int 1)
+     (if (= 0 (mod int 2))
+       (let [new-int (quot int 2)]
+         (if (> new-int 1)
+           (conj (conj factors 2) new-int)
+           (conj factors 2)))
+       (conj factors int))
+     factors)))
